ccfa42ce0ebb1a6cacc8595d027ec7c6cfbac650
emadmuzaffar-commits <emad.muzaffar@wheelergmail.org>
2025-10-06 10:26:57 -0400

    Started writing first test code for odo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTest.java
@@ -1,4 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
-public class FirstTest {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class FirstTest extends LinearOpMode {
+
+    // Declare motors
+    DcMotor FLMotor;
+    DcMotor FRMotor;
+    DcMotor BLMotor;
+    DcMotor BRMotor;
+
+    @Override
+    public void runOpMode() {
+
+        FLMotor = hardwareMap.get(DcMotor.class, "FLMotor");
+        FRMotor = hardwareMap.get(DcMotor.class, "FRMotor");
+        BLMotor = hardwareMap.get(DcMotor.class, "BLMotor");
+        BRMotor = hardwareMap.get(DcMotor.class, "BRMotor");
+
+        // Directions
+        FRMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        BLMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Encoders
+        FLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Brakes
+        FLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // Write the rest of code...
+
+
+
+        }
+    }
 }
