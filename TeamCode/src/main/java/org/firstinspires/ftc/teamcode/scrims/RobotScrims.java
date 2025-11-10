836907a544d44f918f075402879be95c18067cd5
Taka <takabingo2009@gmail.com>
2025-11-10 09:57:45 -0500

    PREPARE FOR SCRIMS

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/RobotScrims.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.scrims;
+
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
+
+public class RobotScrims {
+    public DcMotorEx leftFront, rightFront, leftBack, rightBack;
+    public DcMotorEx flyRight, flyLeft, intake;
+    public DcMotorEx belt;
+    public ServoImplEx onRamp, offRamp;
+    public RobotScrims(HardwareMap hardwareMap){
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+
+        rightFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/ScrimTeleOp.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.scrims;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Configurable
+@TeleOp(name = "Scrims TeleOP", group = "TeleOp")
+public class ScrimTeleOp {
+    
+}
