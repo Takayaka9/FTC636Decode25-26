ee2259f89d3d06d765d27cc388742b513c5a06c9
Taka <takabingo2009@gmail.com>
2025-11-12 21:17:29 -0500

    Update ScrimAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/ScrimAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/ScrimAuto.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.scrims;
 
 import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -8,6 +10,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Configurable
 @Autonomous(name = "ScrimAuto", group = "Autonomous")
 public class ScrimAuto extends LinearOpMode {
+    Follower follower;
+    TelemetryManager TelemetryM;
+    RobotScrims robot = new RobotScrims(hardwareMap);
     @Override
     public void runOpMode() throws InterruptedException {
         waitForStart();
