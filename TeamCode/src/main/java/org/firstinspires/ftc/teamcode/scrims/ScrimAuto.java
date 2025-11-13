2b845c8edbf659525b94188ac4fb8554edb11a71
Taka <takabingo2009@gmail.com>
2025-11-12 23:32:21 -0500

    LL testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/ScrimAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/ScrimAuto.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.scrims;
 
 import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.follower.Follower;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -11,10 +12,19 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name = "ScrimAuto", group = "Autonomous")
 public class ScrimAuto extends LinearOpMode {
     Follower follower;
-    TelemetryManager TelemetryM;
-    RobotScrims robot = new RobotScrims(hardwareMap);
+    TelemetryManager telemetryM;
+    RobotScrims robot;
     @Override
     public void runOpMode() throws InterruptedException {
+        robot = new RobotScrims(hardwareMap);
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+
+
         waitForStart();
+
+        while(opModeIsActive()){
+            telemetryM.update();
+            follower.update();
+        }
     }
 }
