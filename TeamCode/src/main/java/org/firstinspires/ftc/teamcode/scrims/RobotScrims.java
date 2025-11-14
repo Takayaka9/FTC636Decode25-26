3cd430a32841d92de05e25cb33a6c230aa3bb56e
Taka <takabingo2009@gmail.com>
2025-11-13 21:13:42 -0500

    Update RobotScrims.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/RobotScrims.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/RobotScrims.java
@@ -32,19 +32,21 @@ public class RobotScrims {
     public RobotScrims(HardwareMap hardwareMap){
         flyRight = hardwareMap.get(DcMotorEx.class, "flyRight");
         flyLeft = hardwareMap.get(DcMotorEx.class, "flyLeft");
-        intake = hardwareMap.get(DcMotorEx.class, "intakeMotor");
+        flyRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        flyLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        flyRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        flyLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        /*intake = hardwareMap.get(DcMotorEx.class, "intakeMotor");
         belt = hardwareMap.get(DcMotorEx.class, "beltMotor");
 
         onRamp = hardwareMap.get(Servo.class, "onRamp");
         offRamp = hardwareMap.get(Servo.class, "offRamp");
 
-        flyRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        flyLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
         intake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
         belt.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        flyRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        flyLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+
         intake.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         belt.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
@@ -52,6 +54,7 @@ public class RobotScrims {
         distanceSensor = hardwareMap.get(DistanceSensor.class, "CDSensor");
 
         limelight3A = hardwareMap.get(Limelight3A.class, "limelight");
+         */
     }
 
     //initial positions of everything at the start of teleop, as add needed
