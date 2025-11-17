02432eaa8768039ae9d402a66ee96d579063d019
emadmuzaffar-commits <emad.muzaffar@wheelergmail.org>
2025-11-16 23:04:21 -0500

    Made intakerun subsystem and package for command base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/RobotScrims.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/RobotScrims.java
@@ -29,11 +29,12 @@ public class RobotScrims {
     public static int onRampPush = 0;
     public static int offRampPassive = 0;
     public static int offRampPush = 0;
+    public static int intakePower = 0;
     public static PIDFControl_ForVelocity shootControl = new PIDFControl_ForVelocity(0.0, 0.0, 0.0, 0.0); //TODO: TUNE PIDF VALUES
     public RobotScrims(HardwareMap hardwareMap){
         flyRight = hardwareMap.get(DcMotorEx.class, "flyRight");
         flyLeft = hardwareMap.get(DcMotorEx.class, "flyLeft");
-        //intake = hardwareMap.get(DcMotorEx.class, "intakeMotor");
+        intake = hardwareMap.get(DcMotorEx.class, "intakeMotor");
         //belt = hardwareMap.get(DcMotorEx.class, "beltMotor");
 
         //onRamp = hardwareMap.get(Servo.class, "onRamp");
@@ -41,12 +42,12 @@ public class RobotScrims {
 
         flyRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         flyLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        //intake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
+        intake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.FLOAT);
         //belt.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         flyRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         flyLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        //intake.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         //belt.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
         //colorSensor = hardwareMap.get(ColorSensor.class, "CDSensor");
@@ -82,6 +83,11 @@ public class RobotScrims {
     public void pushOn(){
         offRamp.setPosition(offRampPush);
         offRamp.setPosition(offRampPassive);
+    }
+
+    //run intake
+    public void intakeRun(){
+        intake.setPower(intakePower);
     }
 
     //checks if a ball is on the distance sensor
