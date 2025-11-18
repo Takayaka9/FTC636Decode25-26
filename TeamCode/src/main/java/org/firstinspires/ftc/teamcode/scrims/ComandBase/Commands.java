407721c6d60cb4e6026ff681062613ae1c04340c
MWR <118232870+MWRevamped@users.noreply.github.com>
2025-11-18 10:47:58 -0500

    Commands File for future commands :>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/ComandBase/Commands.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.scrims.ComandBase;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+public class Commands {
+
+    /* RunIntake Command */
+    public class RunIntakeCommand extends CommandBase {
+        @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+        private final IntakeSubsystem m_intakesubsystem;
+
+        public RunIntakeCommand(IntakeSubsystem subsystem) {
+            m_intakesubsystem = subsystem;
+            addRequirements(subsystem);
+        }
+
+        @Override
+        public void execute() {
+            m_intakesubsystem.IntakeSubsystemRun();
+        }
+
+        @Override
+        public void end(boolean interrupted) {
+            m_intakesubsystem.IntakeSubsystemStop();
+        }
+
+        public boolean isFinished() {
+            return true;
+        }
+
+
+    }
+}
