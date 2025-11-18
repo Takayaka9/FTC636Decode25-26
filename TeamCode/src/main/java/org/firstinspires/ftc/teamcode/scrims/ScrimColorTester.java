14167ddd317196335356039eedd5ee0274b760cf
Taka <takabingo2009@gmail.com>
2025-11-18 08:56:21 -0500

    Update ScrimColorTester.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/ScrimColorTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scrims/ScrimColorTester.java
@@ -1,12 +1,8 @@
-package org.firstinspires.ftc.teamcode.scrims.ComandBase;
+package org.firstinspires.ftc.teamcode.scrims;
 
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.robot.Robot;
-
-import org.firstinspires.ftc.teamcode.scrims.RobotScrims;
 
 public class ScrimColorTester extends OpMode {
     RobotScrims robot;
