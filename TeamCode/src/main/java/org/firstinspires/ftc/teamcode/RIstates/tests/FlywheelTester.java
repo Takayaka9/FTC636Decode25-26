e32df12d29e4dcaa97d57a475fd8a5de8aa78959
Taka <takabingo2009@gmail.com>
2026-01-07 17:04:25 -0500

    removed one line

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RIstates/tests/FlywheelTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RIstates/tests/FlywheelTester.java
@@ -14,7 +14,6 @@ public class FlywheelTester extends OpMode {
     /// set in panels (pedro units, 12u = 1ft)
     private static double tps = 0;
     SystemManager manager;
-    HardwareMap hardwareMap;
     @Override
     public void init() {
         manager = new SystemManager(hardwareMap, telemetry, gamepad1, gamepad2, true);
