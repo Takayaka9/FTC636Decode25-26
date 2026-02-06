78e4bf203218c652eea461a5461b723abee41c5b
Taka <takabingo2009@gmail.com>
2026-02-06 08:36:10 -0500

    offset code should work now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RIstates/management/handlers/TeleOpHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RIstates/management/handlers/TeleOpHandler.java
@@ -104,13 +104,13 @@ public class TeleOpHandler {
             }
         }
 
-        if (gamepad2.dpad_down && changedDown) {
+        if (gamepad2.dpad_down && !changedDown) {
             changedDown = true;
             offset = offset - 20;
         } if (!gamepad2.dpad_down && changedDown) {
             changedDown = false;
         }
-        if (gamepad2.dpad_up && changedUp) {
+        if (gamepad2.dpad_up && !changedUp) {
             changedUp = true;
             offset = offset + 20;
         } if (!gamepad2.dpad_up && changedUp) {
