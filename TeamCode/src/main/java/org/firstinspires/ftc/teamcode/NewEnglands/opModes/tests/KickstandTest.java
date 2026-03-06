6f61ac3de85c55778b90232e41c7fd20ba1bbf26
Taka <takabingo2009@gmail.com>
2026-03-06 13:48:19 -0500

    errors fucking shit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewEnglands/opModes/tests/KickstandTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewEnglands/opModes/tests/KickstandTest.java
@@ -1,22 +1,25 @@
 package org.firstinspires.ftc.teamcode.NewEnglands.opModes.tests;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
-
+@Configurable
+@TeleOp
 public class KickstandTest extends OpMode {
-    Servo lift1;
-    Servo lift2;
-    public static double lift1Pos;
-    public static double lift2Pos;
+    Servo liftLeft;
+    Servo liftRight;
+    public static double liftLeftPos;
+    public static double liftRightPos;
     @Override
     public void init() {
-        lift1 = hardwareMap.get(Servo.class, "lift1");
-        lift2 = hardwareMap.get(Servo.class, "lift2");
+        liftLeft = hardwareMap.get(Servo.class, "liftLeft");
+        liftRight = hardwareMap.get(Servo.class, "liftRight");
     }
 
     @Override
     public void loop() {
-        lift1.setPosition(lift1Pos);
-        lift2.setPosition(lift2Pos);
+        liftLeft.setPosition(liftLeftPos);
+        liftRight.setPosition(liftRightPos);
     }
 }
