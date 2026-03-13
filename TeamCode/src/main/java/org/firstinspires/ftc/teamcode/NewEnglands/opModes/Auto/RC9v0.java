4c615e841828b7cbb3642bbedbd04aaf4203c0eb
emadmuzaffar-commits <emad.muzaffar@wheelergmail.org>
2026-03-13 18:16:27 +0100

    auto done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewEnglands/opModes/Auto/RC9v0.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewEnglands/opModes/Auto/RC9v0.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.NewEnglands.opModes.Auto;
 
-public class RC9v0 {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.NewEnglands.pedro.pathUpdates.C9PathUpdate;
+import org.firstinspires.ftc.teamcode.NewEnglands.utils.alliance.Alliance;
+import org.firstinspires.ftc.teamcode.NewEnglands.utils.pedroUtils.PathUpdate;
+
+@Autonomous
+public class RC9v0 extends OpMode {
+
+    PathUpdate pathUpdate;
+
+    @Override
+    public void init() {
+        pathUpdate = new C9PathUpdate(Alliance.RED, hardwareMap, telemetry);
+        pathUpdate.init();
+    }
+
+    @Override
+    public void start() {
+        pathUpdate.start();
+    }
+
+    @Override
+    public void loop() {
+        pathUpdate.update();
+    }
+
 }
