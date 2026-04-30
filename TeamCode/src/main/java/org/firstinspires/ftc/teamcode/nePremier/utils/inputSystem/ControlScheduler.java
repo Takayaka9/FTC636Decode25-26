8475bdf0f102b5f810d4d4b91b21fe3ef54fcf75
emadmuzaffar-commits <emad.muzaffar@wheelergmail.org>
2026-04-30 19:14:47 -0400

    control scheduler

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nePremier/utils/inputSystem/ControlScheduler.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.nePremier.utils.inputSystem;
+
+public class ControlScheduler extends Builder {
+    //instance of single class
+    public static ControlScheduler instance = null;
+
+    //private constructor so that it can't be initialized normally
+    private ControlScheduler() {}
+
+     /**
+    getInstance is a method that returns the ControlScheduler singleton,
+    throws IllegalAccessException if scheduler was not initialized automatically
+      **/
+    public static synchronized ControlScheduler getInstance() throws IllegalAccessException {
+        if (instance != null) return instance;
+        else throw new IllegalAccessException("what the fuck did you even code");
+    }
+
+    /// Required to function, updates the controls
+    public void update() {
+        controls.forEach(control -> update());
+    }
+
+}
