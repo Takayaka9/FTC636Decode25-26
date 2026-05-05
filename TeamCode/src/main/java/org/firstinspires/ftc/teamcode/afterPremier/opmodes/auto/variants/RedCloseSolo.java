f4c37fcdebed12eeb3cd6b1affee459db405c5ad
Taka <takabingo2009@gmail.com>
2026-05-05 09:31:16 -0400

    another auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/afterPremier/opmodes/auto/variants/RedCloseSolo.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.afterPremier.opmodes.auto.variants;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.afterPremier.opmodes.auto.CloseSolo;
+import org.firstinspires.ftc.teamcode.afterPremier.util.Alliance;
+@Autonomous
+public class RedCloseSolo extends CloseSolo {
+    public RedCloseSolo(){
+        super(Alliance.RED);
+    }
+}
